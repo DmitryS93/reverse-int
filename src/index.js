33f81f55784fa927b03ba19cb911479fe49c5456
DmitryS93 <119356125+DmitryS93@users.noreply.github.com>
2023-01-12 09:20:16 +0300

    Update: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    const nAbs = Math.abs(n)
+    const nString = String(nAbs);
+    let result = ""
+    for (let i = 0; i < nString.length; i += 1) {
+        result = nString[i] + result;
+    }
+    return result
 }
